768d48c0a186a165172bc7a6d68071b4d45e9664
Farrah Ryan <ryanfarrah25@yahoo.fr>
2017-02-13 11:10:08 +0100

    Delete useless information

--- index.php
+++ index.php
@@ -12,15 +12,7 @@
     
     <body>
     
-          <a class='dropdown-button btn' href='#' data-activates='dropdown1'>Drop Me!</a>
-
-  <!-- Dropdown Structure -->
-  <ul id='dropdown1' class='dropdown-content'>
-    <li><a href="#!">one</a></li>
-    <li><a href="#!">two</a></li>
-    <li class="divider"></li>
-    <li><a href="#!">three</a></li>
-  </ul>
+        <a class='dropdown-button btn' href='#' data-activates='dropdown1'>Drop Me!</a>
     
     <script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.1.1/jquery.js"></script>
     <script src="https://cdnjs.cloudflare.com/ajax/libs/materialize/0.98.0/js/materialize.js"></script>

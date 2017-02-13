b23b9a63f0225dbe75f95e0972258e0efd4bbcf0
Farrah Ryan <ryanfarrah25@yahoo.fr>
2017-02-13 11:09:00 +0100

    Index.php CDN links materialize

---
+++ index.php
@@ -0,0 +1,29 @@
+<!DOCTYPE html>
+
+<html>
+
+<head>
+    
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/materialize/0.98.0/css/materialize.css"/>
+    <meta charset="utf-8"/>
+    <title>Face_Generator_Solo</title>
+    
+</head>
+    
+    <body>
+    
+          <a class='dropdown-button btn' href='#' data-activates='dropdown1'>Drop Me!</a>
+
+  <!-- Dropdown Structure -->
+  <ul id='dropdown1' class='dropdown-content'>
+    <li><a href="#!">one</a></li>
+    <li><a href="#!">two</a></li>
+    <li class="divider"></li>
+    <li><a href="#!">three</a></li>
+  </ul>
+    
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.1.1/jquery.js"></script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/materialize/0.98.0/js/materialize.js"></script>
+    </body>
+    
+</html>

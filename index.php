b706949b9da8a23ac7a1ecac00259fdfae562734
Farrah Ryan <ryanfarrah25@yahoo.fr>
2017-02-13 16:18:55 +0100

    Ryan stable version v1

--- index.php
+++ index.php
@@ -34,17 +34,17 @@
         
     </div>
         
-<div class="container">
-    <div class="face"> 
+<div class="container" style="height: 30%">
+    <div class="face" style="height: 10%"> 
         
-        <img src="view/img/hairs/hairs_1.png" /> 
-        <img src="view/img/hairs/hairs_2.png" /> 
-        <img src="view/img/hairs/hairs_3.png" /> 
-        <img src="view/img/hairs/hairs_4.png" /> 
-        <img src="view/img/hairs/hairs_5.png" /> 
-        <img src="view/img/hairs/hairs_5.png" /> 
-        <img src="view/img/hairs/hairs_7.png" /> 
-        <img src="view/img/hairs/hairs_8.png" /> 
+        <img src="view/img/hairs/hairs_1.png" !important/> 
+        <img src="view/img/hairs/hairs_2.png" !important/> 
+        <img src="view/img/hairs/hairs_3.png" !important/> 
+        <img src="view/img/hairs/hairs_4.png" !important/> 
+        <img src="view/img/hairs/hairs_5.png" !important/> 
+        <img src="view/img/hairs/hairs_5.png" !important/> 
+        <img src="view/img/hairs/hairs_7.png" !important/> 
+        <img src="view/img/hairs/hairs_8.png" !important/> 
     </div>
     
     <div class="face"> 
@@ -53,12 +53,41 @@
         <img src="view/img/eyes/eyes_2.png" /> 
         <img src="view/img/eyes/eyes_3.png" /> 
         <img src="view/img/eyes/eyes_4.png" /> 
-        <img src="view/img/eyes/eyes_5.png" /> 
-        <img src="view/img/eyes/eyes_6.png" /> 
+        <img src="view/img/eyes/eyes_5.png" />  
         <img src="view/img/eyes/eyes_7.png" /> 
         <img src="view/img/eyes/eyes_8.png" /> 
         <img src="view/img/eyes/eyes_9.png" /> 
         <img src="view/img/eyes/eyes_10.png" /> 
+        
+    </div>
+    
+    <div class="face"> 
+        
+        <img src="view/img/nose/nose_1.png" /> 
+        <img src="view/img/nose/nose_2.png" /> 
+        <img src="view/img/nose/nose_3.png" /> 
+        <img src="view/img/nose/nose_4.png" /> 
+        <img src="view/img/nose/nose_5.png" /> 
+        <img src="view/img/nose/nose_6.png" /> 
+        <img src="view/img/nose/nose_7.png" /> 
+        <img src="view/img/nose/nose_8.png" /> 
+        <img src="view/img/nose/nose_9.png" /> 
+        <img src="view/img/nose/nose_10.png" /> 
+        
+    </div>
+    
+    <div class="face"> 
+        
+        <img src="view/img/mouth/mouth_1.png" /> 
+        <img src="view/img/mouth/mouth_2.png" /> 
+        <img src="view/img/mouth/mouth_3.png" /> 
+        <img src="view/img/mouth/mouth_4.png" /> 
+        <img src="view/img/mouth/mouth_5.png" /> 
+        <img src="view/img/mouth/mouth_6.png" /> 
+        <img src="view/img/mouth/mouth_7.png" /> 
+        <img src="view/img/mouth/mouth_8.png" /> 
+        <img src="view/img/mouth/mouth_9.png" /> 
+        <img src="view/img/mouth/mouth_10.png" /> 
         
     </div>
     
@@ -72,10 +101,7 @@
         
         
      <script type="text/javascript">
-    $(document).ready(function(){
-      $('.face').slick({
-      });
-    });
+   
   </script>
         
         

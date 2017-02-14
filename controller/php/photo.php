8a6ea3de4d10329ebfdce1d1e88e6f039d55b266
Farrah Ryan <ryanfarrah25410@gmail.com>
2017-02-14 15:59:15 +0100

    Push for cohesion Manu

--- controller/php/photo.php
+++ controller/php/photo.php
@@ -1,4 +1,9 @@
 <?php
-	$save = str_replace('data:image/png;base64,', '', $_POST['#photo'] );
-	file_put_contents( '../../view/img/face/image.png', base64_decode( $save ) );
+
+'include ../../model/pdo.php'
+
+
+    $save = str_replace('data:image/png;base64,', '', $_POST['#photo'] );
+    file_put_contents( '../../view/img/face/image.png', base64_decode( $save ) );
+
 ?>

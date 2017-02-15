261beca427289f4df22fd8ab88143b0ce0c89af2
Unknown <manu_guillou@yahoo.fr>
2017-02-15 09:32:23 +0100

    delete useless image and stage photo request update

--- controller/php/photo.php
+++ controller/php/photo.php
@@ -1,4 +1,12 @@
 <?php
-	$save = str_replace('data:image/png;base64,', '', $_POST['#photo'] );
-	file_put_contents( '../../view/img/face/image.png', base64_decode( $save ) );
+	include"PDO.php";
+	$query = $dbh->query("SELECT head.id_face FROM head");
+	$result = $query->fetchAll();
+	print_r($result);
+	$number = count($result);
+	$number = ++$number;
+	$save = str_replace('data:image/png;base64,', '', $_POST['#photo']);
+	file_put_contents( "../../view/img/face/$number.png", base64_decode($save));
+	$req = $dbh->query("INSERT INTO head (face) VALUES ('view/img/face/".$number.".png')");
+	echo "toto";
 ?>

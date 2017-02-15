1117c9b17f88f50b087ed79771a782535bf925d2
Unknown <manu_guillou@yahoo.fr>
2017-02-15 10:59:01 +0100

    debug index

--- controller/php/photo.php
+++ controller/php/photo.php
@@ -8,5 +8,4 @@
 	$save = str_replace('data:image/png;base64,', '', $_POST['#photo']);
 	file_put_contents( "../../view/img/face/$number.png", base64_decode($save));
 	$req = $dbh->query("INSERT INTO head (face) VALUES ('view/img/face/".$number.".png')");
-	echo "toto";
 ?>

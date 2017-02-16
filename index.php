9233f00a2b664d510f43451e848e3376a14c96bb
Mathieucst <cressot.mathieu@gmail.com>
2017-02-16 11:14:34 +0100

    fb test

--- index.php
+++ index.php
@@ -139,7 +139,7 @@ $response->closeCursor(); ?>
 
 <g:plus action="share"></g:plus>
 
-<div class="fb-share-button" data-href="http://ryanf.marmier.codeur.online/February/Work/Team/avatar-generator/" data-layout="button_count" data-size="small" data-mobile-iframe="true"><a class="fb-xfbml-parse-ignore" target="_blank" href="https://www.facebook.com/sharer/sharer.php?u=http%3A%2F%2Fryanf.marmier.codeur.online%2FFebruary%2FWork%2FTeam%2Favatar-generator%2F&amp;src=sdkpreparse">Partager</a></div>
+<div class="fb-share-button" data-href="http://quangb.marmier.codeur.online/avatar-generator/" data-layout="button_count" data-size="small" data-mobile-iframe="true"><a class="fb-xfbml-parse-ignore" target="_blank" href="https://www.facebook.com/sharer/sharer.php?u=http%3A%2F%2Fryanf.marmier.codeur.online%2FFebruary%2FWork%2FTeam%2Favatar-generator%2F&amp;src=sdkpreparse">Partager</a></div>
         
         
     <script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.1.1/jquery.js"></script>

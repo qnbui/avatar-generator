661880f3330a42c02fb31681d0a63a4ce160ce2c
Unknown <manu_guillou@yahoo.fr>
2017-02-16 14:12:19 +0100

    image resize ok

--- controller/php/photo.php
+++ controller/php/photo.php
@@ -8,4 +8,12 @@
 	$save = str_replace('data:image/png;base64,', '', $_POST['#photo']);
 	file_put_contents( "../../view/img/face/$number.png", base64_decode($save));
 	$req = $dbh->query("INSERT INTO head (face) VALUES ('view/img/face/".$number.".png')");
+	// include composer autoload
+	require '../../vendor/autoload.php';
+	// import the Intervention Image Manager Class
+	use Intervention\Image\ImageManager;
+	// create an image manager instance with favored driver
+	$manager = new ImageManager();
+	// to finally create image instances
+	$manager->make('../../view/img/face/'.$number.'.png')->resize(275, 320)->save('../../view/img/face/'.$number.'.png');
 ?>

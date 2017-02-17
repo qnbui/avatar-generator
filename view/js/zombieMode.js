ecc6422cff1d5da5ff4d4a21a4f9e85c217d2963
Farrah Ryan <ryanfarrah25410@gmail.com>
2017-02-17 15:31:25 +0100

    BUTTON WORKS

--- view/js/zombieMode.js
+++ view/js/zombieMode.js
@@ -1,6 +1,23 @@
 $(document).ready(function() {
     $("#zombieModeButton").click(function()
     {
-      $(".zombieModeDiv").toggleClass( ".zombieModeDivTG" );
+        console.log("dsqd");
+      $(".zombieModeDiv").toggleClass( "zombieModeDivTG" );
     });
 });
+
+/* $(document).ready(function() {
+    $("#zombieModeButton").click(function()
+    {
+       if($(#zombieModeDiv).backgroundColor() == "transparent"
+       {
+            $(#zombieModeDiv).css({backgroundColor: "rgba(0, 255, 0, 0.3)"})
+       }
+       else
+       {
+        $(#zombieModeDiv).css({backgroundColor: "transparent"})
+       }
+    });
+});
+
+
